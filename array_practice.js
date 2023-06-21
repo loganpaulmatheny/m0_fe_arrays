a31642869343f107dcdaebe0b66d6dc4a762bce3
Logan Matheny <loganpaulmatheny@gmail.com>
2023-06-21 15:35:30 -0400

    Initial commit

---
+++ array_practice.js
@@ -0,0 +1,10 @@
+var friends = ["Arlo", "Nick", "Timmy", "Tyler", "Ted"];
+// friends.pop();
+// console.log(friends);
+
+// friends.push("Drew");
+// console.log(friends);
+
+// var arr1 = friends.shift();
+// console.log(friends);
+// console.log(arr1);
