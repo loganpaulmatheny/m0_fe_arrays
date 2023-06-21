9f90b83bc47bec5e5f982bae42cc2e0a75725b3e
Logan Matheny <loganpaulmatheny@gmail.com>
2023-06-21 15:46:45 -0400

    Demonstrate understanding of indexes in arrays

--- arrays.js
+++ arrays.js
@@ -4,14 +4,24 @@ var arrNumbers = [1, 2, 3, 4];
 
 var arrBool = [true, false, false, true];
 
-// push the string "Oly" to the end of the strings array
-arrString.push("Oly");
-console.log(arrString);
+// // push the string "Oly" to the end of the strings array
+// arrString.push("Oly");
+// console.log(arrString);
 
-// returns the end two elements from the numbers array
-var endNumbers = arrNumbers.slice(-2);
-console.log(endNumbers);
+// // returns the end two elements from the numbers array
+// var endNumbers = arrNumbers.slice(-2);
+// console.log(endNumbers);
 
-// add false element to the beginning of the array
-arrBool.unshift(false);
-console.log(arrBool);
+// // add false element to the beginning of the array
+// arrBool.unshift(false);
+// console.log(arrBool);
+
+// EXAMPLE OF INDEX POSITIONS
+// arrString[2] should refer to "Logan"
+console.log(arrString[2]);
+// arrNumbers[0] should refer to 1
+console.log(arrNumbers[0]);
+// arrBool[3] should refer to true
+console.log(arrBool[3]);
+
+// EXPLANATION: Arrays operate as a 0 index list meaning, the first element in the array represents the 0 position.
