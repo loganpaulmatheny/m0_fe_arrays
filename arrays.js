71a06600cabd1a7b4a0f9d751fa8d1f0cfd73460
Logan Matheny <loganpaulmatheny@gmail.com>
2023-06-21 15:42:23 -0400

    Adds three examples of array methods

--- arrays.js
+++ arrays.js
@@ -3,3 +3,15 @@ var arrString = ["Arlo", "Lily", "Logan", "Delphine"];
 var arrNumbers = [1, 2, 3, 4];
 
 var arrBool = [true, false, false, true];
+
+// push the string "Oly" to the end of the strings array
+arrString.push("Oly");
+console.log(arrString);
+
+// returns the end two elements from the numbers array
+var endNumbers = arrNumbers.slice(-2);
+console.log(endNumbers);
+
+// add false element to the beginning of the array
+arrBool.unshift(false);
+console.log(arrBool);
